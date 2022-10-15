d0aff4e07d7d254756d961c632b802605bfd2f7e
ricardocandelo <richar07disdan@gmail.com>
2022-10-15 18:00:00 -0500

    Proyecto1

---
+++ foot.php
@@ -0,0 +1,5 @@
+<footer>
+    <h1>Este es el footer</h1>
+</footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+include ("header.php");
+?>
+<form action="Agenda.php" method="post"></form>
+<input type="submit" value="N">
+
+<?php
+include ("footer.php");
+?>
