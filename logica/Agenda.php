e0a040e229862098db94d90097da44c0d27551b4
ricardocandelo <richar07disdan@gmail.com>
2022-10-16 13:23:30 -0500

    conexion

---
+++ logica/Agenda.php
@@ -0,0 +1,19 @@
+<?php
+    class votos extends modeloCredencialesDB{
+
+        public function __autoload() {
+            parent::__construct();
+        }
+
+        public function listar_notas(){}
+        
+        public function ver_nota(){}
+
+        public function nueva_nota(){}
+
+        public function eliminar_nota(){}
+
+        public function editar_nota(){}
+
+    }
+?>
